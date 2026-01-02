b3ade03d6234f518a5639a8f383d77b6bde1ac40
vikramrajendraprasad-ops <vikramrajendraprasad@gmail.com>
2026-01-02 20:50:14 +0530

    MainActivity.kt

--- audio_cinema_studio/android/app/src/main/kotlin/com/example/audio_cinema_studio/MainActivity.kt
+++ audio_cinema_studio/android/app/src/main/kotlin/com/example/audio_cinema_studio/MainActivity.kt
@@ -1,5 +1,29 @@
-package com.example.audio_cinema_studio
+package com.yourbrand.audiocinemastudio
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "audio_cinema/engine"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "setCinemaConfig" -> {
+                    val args = call.arguments as Map<*, *>
+                    Log.d("AudioCinema", "Received config: $args")
+                    result.success("Config received successfully")
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+}

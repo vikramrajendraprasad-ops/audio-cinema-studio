b631ba98c23930e5913efb261bfcac322199926b
vikramrajendraprasad-ops <vikramrajendraprasad@gmail.com>
2026-01-03 08:16:08 +0530

    Delete audio_cinema_studio/audio_cinema_studio directory

--- audio_cinema_studio/audio_cinema_studio/android/app/src/main/kotlin/com/example/audio_cinema_studio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.audio_cinema_studio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- audio_cinema_studio/audio_cinema_studio/android/app/src/main/kotlin/com/yourbrand/audiocinemastudio/audio_cinema_studio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yourbrand.audiocinemastudio.audio_cinema_studio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
